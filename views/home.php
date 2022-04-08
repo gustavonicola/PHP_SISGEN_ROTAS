293abd96d63ccf1cbd56c6a8bc2cf0e8c2e0ff47
Gustavo Nicola <nicolajau@hotmail.com>
2022-04-08 11:18:00 -0300

    Teste de funcionalidade do get no VS Code

--- views/home.php
+++ views/home.php
@@ -7,8 +7,8 @@
         
         <?php include 'includes/cabecalho.php'; ?>
 
-        <main>
-            Tela inicial
+        <main class="container">
+            <h2>Dashboard</h2>
         </main>
         
         <?php include 'includes/rodape.php'; ?>
